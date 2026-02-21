53b3c99cedcf7a9e1de21107c27c85a41b999480
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-02-22 05:09:53 +0800

    Add frontend/src/App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { MainPage } from './components/MainPage';
+
+const App: React.FC = () => (
+  <Provider store={store}>
+    <MainPage />
+  </Provider>
+);
+
+export default App;
