6454474151feb02db447122a42fa5902d187ce9f
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-02-22 08:04:56 +0800

    Add infrastructure/terraform/main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,25 +2,14 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_ecs_cluster" "example" {
-  name = "my-cluster"
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "multi-model-orchestration-cluster"
 }
 
-resource "aws_ecs_service" "service" {
-  name            = "my-service"
-  cluster         = aws_ecs_cluster.example.id
-  desired_count   = 1
+resource "aws_ecs_service" "fastapi_service" {
+  name            = "fastapi-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  desired_count   = 2
   launch_type     = "FARGATE"
-
-  task_definition = aws_ecs_task_definition.example.arn
-}
-
-resource "aws_ecs_task_definition" "example" {
-  family                   = "service"
-  container_definitions    = jsonencode([{
-    name  = "web"
-    image = "nginx"
-    cpu   = 256
-    memory = 512
-  }])
+  task_definition = aws_ecs_task_definition.fastapi_task.arn
 }
